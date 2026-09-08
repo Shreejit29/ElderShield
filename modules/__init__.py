6fba7b59f927c0a8543254723d99d977d7b4cfc6
Shreejit29 <udekarp007@gmail.com>
2026-09-08 14:26:42 +0530

    Create __init__.py

---
+++ modules/__init__.py
@@ -0,0 +1,9 @@
+"""
+ElderShield modules package.
+
+Safety-first analysis and protection components for the ElderShield
+anti-phishing and anti-scam application.
+"""
+
+__version__ = "5.0.0"
+__all__ = []
